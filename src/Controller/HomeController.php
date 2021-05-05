d7b7decbabd05a6bbe399d3e576b35382fa71364
Grafé <Sophie>
2021-05-05 18:28:17 +0200

    template integration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,37 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/home/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('home/about/about.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home/blogposts', name: 'blogposts')]
+    public function blogposts(): Response
+    {
+        return $this->render('home/blogposts/blogposts.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home/properties', name: 'properties')]
+    public function properties(): Response
+    {
+        return $this->render('home/properties/properties.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/home/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact/contact.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
 }
