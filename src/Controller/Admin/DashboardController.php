cf9810f8088ed6cf955e62575359a6dbd5a0be6c
Grafé <Sophie>
2021-05-22 21:29:03 +0200

    add user crud to admin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Entity\Blogpost;
 use App\Entity\Property;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Security\Permission;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -30,5 +32,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Blogposts', 'fas fa-newspaper', Blogpost::class);
         yield MenuItem::linkToCrud('Properties', 'fas fa-igloo', Property::class);
+        yield MenuItem::linkToCrud('Users', 'fas fa-users', User::class);
     }
 }
